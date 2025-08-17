18a50522e3b0367a6ffb8f7a5acff3cf5ee83ebf
ShrutiPandey-4 <23053394@kiit.ac.in>
2025-08-17 13:30:24 +0530

    Update 127_0_0_1.sql

--- build/classes/Mysql/127_0_0_1.sql
+++ build/classes/Mysql/127_0_0_1.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Aug 27, 2022 at 12:54 PM
--- Server version: 5.7.36
--- PHP Version: 7.4.26
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
